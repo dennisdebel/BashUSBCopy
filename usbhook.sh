be27415be4727f8b637ea96a7bab5d62f9fab369
dennisdebel <dennisdebel@gmail.com>
2021-03-18 19:29:00 +0100

    reversed the functionality of usbhook
    it now copies files from usb to your hdd instead of the other way around

--- usbhook.sh
+++ usbhook.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#################################################
-#						#
-# Automatic copy on usb drive when plugged !	#
-#                      	    Célestin PREAULT	#
-#						#
-#################################################
+#########################################################
+#							#
+# Automatic copy contents from usb drive when plugged ! #
+#                      	    Célestin PREAULT		#
+#		            Mod by Dennis de Bel	#
+#########################################################
 
 # Please add that command in /etc/udev/rules.d/99-usbhook.rules to run script when a usb drive is plugged in
 #
-#	ACTION=="add",KERNEL=="sd*", SUBSYSTEMS=="usb", ATTRS{product}=="*", RUN+="/home/stf/usbhook.sh %k"
+#	ACTION=="add",KERNEL=="sd*", SUBSYSTEMS=="usb", ATTRS{product}=="*", RUN+="/home/USERNAME/usbhook.sh %k"
 #
 #	(Change run command according to the location of that script)
 #
@@ -22,7 +22,10 @@ exec > >(tee -a ${LOG_FILE} )
 exec 2> >(tee -a ${LOG_FILE} >&2)
 
 # CONFIGURATION: Source folder
-SRC="/home/stf/usb_source"
+#SRC="/var/www/usb_source"
+
+# CONFIGURATION: Destination folder (this copies contents from usb drive to your public www folder)
+DEST="/var/www/html"
 
 DEVICE="$1" # the device name
 
@@ -36,10 +39,14 @@ fi
 
 mkdir /tmp/"$DEVICE"
 
-# Mount ntfs usb drive
-mount -t ntfs-3g /dev/"$DEVICE"1 /tmp/"$DEVICE"
+# Mount fat usb drive
+mount -t vfat /dev/"$DEVICE"1 /tmp/"$DEVICE"  -o rw,umask=0000
 
-cp -R "$SRC"/* /tmp/"$DEVICE"
+# Copy file TO usb
+#cp -R "$SRC"/* /tmp/"$DEVICE"
+
+# Copy files FROM usb
+cp -R /tmp/"$DEVICE"/* "$DEST"
 chmod 777 -R /tmp/"$DEVICE"
 
 umount /tmp/"$DEVICE"
@@ -48,6 +55,6 @@ rm -r /tmp/"$DEVICE"
 echo "Copy to $DEVICE  >> Done !"
 
 export DISPLAY=:0
-sudo -u stf notify-send "Copy to $DEVICE >> Done !" -t 5000
+#sudo -u pi notify-send "Copy to $DEVICE >> Done !" -t 5000
 
 exit 0
